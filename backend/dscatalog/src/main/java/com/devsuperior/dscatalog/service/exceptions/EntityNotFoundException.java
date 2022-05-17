c3f3f3f2dabf2b8f012299bb19e5a9f3ab5e1993
rebeka-batista <rebekabatista05@outlook.com>
2022-05-17 19:13:39 -0300

    Find category by id, exception handling

---
+++ backend/dscatalog/src/main/java/com/devsuperior/dscatalog/service/exceptions/EntityNotFoundException.java
@@ -0,0 +1,9 @@
+package com.devsuperior.dscatalog.service.exceptions;
+
+public class EntityNotFoundException extends RuntimeException {
+
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+
+}
